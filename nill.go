1a4bc65ab34f85106d2f4d692c9ad6cb421ec340
gilang-as <me@gilang.dev>
2023-11-08 17:23:15 +0700

    Update comparable type to any

--- nill.go
+++ nill.go
@@ -1,10 +1,10 @@
 package nill
 
-type Type[T comparable] struct {
+type Type[T any] struct {
 	Valid bool
 	Value T
 }
 
-func New[T comparable](value T) Type[T] {
+func New[T any](value T) Type[T] {
 	return Type[T]{Valid: true, Value: value}
 }
